85e65f80890cc41f8b5723d8e9ea92b052f2f899
Maciekskc <maciekskc@gmail.com>
2020-05-24 15:50:47 +0200

    dodanie wątku wypisywania

--- etap2.py
+++ etap2.py
@@ -6,6 +6,7 @@
 import threading
 import random
 import time
+import os
 
 CLIENT_NUMBEROF = 5
 CASHIER_NUMBEROF = 2
@@ -24,6 +25,8 @@ trolleys_queue = []
 
 class Client(threading.Thread):
     running = True
+    position=0
+    next_product=0
     shopping_list = []
   
     def __init__(self,client_id,client_trolley):
@@ -64,36 +67,54 @@ class Client(threading.Thread):
         print(self.shopping_list)
         while len(self.shopping_list)!=0:
             #wyznaczamy następną półke z produktem
-            next_product = self.shopping_list[0]
+            self.next_product = self.shopping_list[0]
             next_target = self.shopping_list[0]//3
             while self.position != next_target:
-                print("Klient "+ str(self.client_id)+ " znajduje sie przy półce "+ str(self.position)+" i kieruje się do półki "+ str(next_target)+" po kolejny produkt")
+                # print("Klient "+ str(self.client_id)+ " znajduje sie przy półce "+ str(self.position)+" i kieruje się do półki "+ str(next_target)+" po kolejny produkt")
                 time.sleep(1)
                 if self.position > next_target:
                     self.position = self.position - 1
                 if self.position < next_target:
                     self.position = self.position+ 1
-            print("Klientowi "+ str(self.client_id) +" prubuje dostać się do półki ")   
+                # print("Klientowi "+ str(self.client_id) +" prubuje dostać się do półki ")   
             locked=shelves[next_target-1].locked
             if locked:
-                print("Klient "+str(self.client_id)+" dotarł do półki "+str(self.position)+", znalazł produkt! Pobiera go z półki i sprawdza czy zostało mu coś jeszcze na liście")
+                # print("Klient "+str(self.client_id)+" dotarł do półki "+str(self.position)+", znalazł produkt! Pobiera go z półki i sprawdza czy zostało mu coś jeszcze na liście")
                 self.shopping_list.pop(0)
                 time.sleep(2)
-                print("Klientowi "+ str(self.client_id) +" pozostało " + str(len(self.shopping_list)) + " produktów!")
+                # print("Klientowi "+ str(self.client_id) +" pozostało " + str(len(self.shopping_list)) + " produktów!")
                 shelves[next_target-1].release
             else:
                 time.sleep(1)
                 self.shopping_list.append(self.shopping_list[0]-1)
                 self.shopping_list.pop(0)
-                print("Klient "+ str(self.client_id) +" dotarł do półki " + str(self.position) + " jednak ktoś przy niej stoi, rusza więc po kolejny produkt")
-        print("Lista zakupów została skompletowana!")
-        exit(0)
+                # print("Klient "+ str(self.client_id) +" dotarł do półki " + str(self.position) + " jednak ktoś przy niej stoi, rusza więc po kolejny produkt")
+        # print("Lista zakupów została skompletowana!")
         pass
+
+    def __str__(self):
+        return("Klient: "+str(self.client_id)+" znajduje się przy półce "+str(self.position)+" i kieruje się do półki "+str(self.next_product//3))
       
 # TODO
 class Cashier():
     # Obsługuje klientów przez czas proporcjonalny do dł. listy zakupów
     pass
+
+
+class Printer(threading.Thread):
+    running = True
+    def __init__(self,client_list):
+        threading.Thread.__init__(self)
+        self.client_list = client_list
+        print("konstruktor W WYPISYWANIU ")
+        
+    def run(self):
+        while self.running:
+            os.system('cls')
+            print("JESTEŚMY W WYPISYWANIU ")
+            for client in self.client_list:
+                print(client)
+            time.sleep(1)
 
 # Główna fukncja programu
 def simulation():
@@ -104,6 +125,9 @@ def simulation():
     Client.running = True
     for c in clients:
         c.start()
+    printer = Printer(clients)
+    printer.running = True 
+    printer.start()
 
 # Start symulacji
 simulation()
